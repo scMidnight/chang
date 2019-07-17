a6d09a8c4ead44c6df25eae1defcf846fb8b4bae
midnight_corp <3741637@qq.com>
2019-07-17 16:27:48 +0800

    修改logback，添加logback测试类测试log输出

---
+++ chang-admin/src/main/java/com/chang/changadmin/web/controller/demo/LogbackDemo.java
@@ -0,0 +1,19 @@
+package com.chang.changadmin.web.controller.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by SunChang on 2019/7/17
+ */
+public class LogbackDemo {
+    private static Logger log = LoggerFactory.getLogger(LogbackDemo.class);
+
+    public static void main(String[] args) {
+        log.trace("========trace");
+        log.debug("========debug");
+        log.info("========info");
+        log.warn("========warn");
+        log.error("========error");
+    }
+}
